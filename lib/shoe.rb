b02ee1f921d697e7f904272677636027cdcfe3ae
Nadia Lee <miss.nadia.lee@gmail.com>
2018-01-17 19:30:15 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,15 @@
-# Make your shoe class here!
+class Book
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :title
+
+  def initialize(brand)
+    @brand = brand
+  end
+
+  def cobble
+    puts "Your shoe is as good as new!"
+    if Shoe.condition == "old"
+      Shoe.condition == "new"
+    end
+  end
+end
