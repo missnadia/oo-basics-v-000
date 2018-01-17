76d0cc350c1cf473a9f3ec53abc206dbff548cc3
Nadia Lee <miss.nadia.lee@gmail.com>
2018-01-17 19:32:08 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,15 +1,13 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
-  attr_reader :condition
+  attr_reader :brand
 
   def initialize(brand)
     @brand = brand
   end
 
   def cobble
+    @condition = "new"
     puts "Your shoe is as good as new!"
-    if Shoe.condition == "old"
-      Shoe.condition == "new"
-    end
   end
 end
